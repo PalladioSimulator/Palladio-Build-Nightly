88b308a2a7f78dc1c98f225a96ed0d2f03d2a7be
Lucas Alber <lucasd.alber@gmail.com>
2023-05-15 14:20:53 +0200

    Add reasoning to github_action_loader

--- scripts/generate_nightly/generate_nightly/yaml/github_action_loader.py
+++ scripts/generate_nightly/generate_nightly/yaml/github_action_loader.py
@@ -4,10 +4,14 @@ from yaml.loader import Composer, Parser, Reader, Resolver, SafeConstructor, Sca
 
 
 class GithubActionResolver(Resolver):
+    """
+    A custom resolver that does not treat "on" as boolean (as per YAML spec),
+    instead it is treated as string instead.
+    """
     @classmethod
     def replace_implicit_resolver(cls, tag, regexp, old_first):
         for ch in old_first:
-            for i, (tag_i, regexp_i) in enumerate(cls.yaml_implicit_resolvers[ch]):
+            for i, (tag_i, _) in enumerate(cls.yaml_implicit_resolvers[ch]):
                 if tag_i == tag:
                     cls.yaml_implicit_resolvers[ch][i] = (tag, regexp)
 
@@ -24,6 +28,10 @@ GithubActionResolver.replace_implicit_resolver(
 class GithubActionSafeLoader(
     Reader, Scanner, Parser, Composer, SafeConstructor, GithubActionResolver
 ):
+    """
+    A custom loader that does not treat "on" as boolean (as per YAML spec),
+    instead it is treated as string instead.
+    """
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)

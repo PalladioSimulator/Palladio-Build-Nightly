ba846b472e2dc0a9afe3e20e1defc4fce7b85548
Lucas Alber <lucasd.alber@gmail.com>
2023-05-28 12:43:42 +0200

    Define templated custom jobs

--- scripts/generate_nightly/generate_nightly/__main__.py
+++ scripts/generate_nightly/generate_nightly/__main__.py
@@ -3,12 +3,13 @@
 import json
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from .template.template import Template
 
 PATH_JOB_TEMPLATE = Path("template/job_template.yml")
 PATH_NIGHTLY_TEMPLATE = Path("template/nightly_template.yml")
+PATH_CUSTOM_JOBS_TEMPLATE = Path("template/custom_jobs_template.yml")
 PATH_NIGHTLY = Path(".github/workflows/nightly.yml")
 
 
@@ -16,8 +17,21 @@ def main():
     repo_dependencies_json = os.environ["DEPENDENCIES"]
     repo_dependencies: Dict[str, List[str]] = json.loads(repo_dependencies_json)
 
-    job_template = Template(PATH_JOB_TEMPLATE)
     jobs = {}
+
+    custom_jobs_template = Template(PATH_CUSTOM_JOBS_TEMPLATE)
+    variables = {
+        "repo_names": list(repo_dependencies.keys()),
+        "repo_names_short": [
+            dep_name.split("/")[1] for dep_name in repo_dependencies.keys()
+        ],
+    }
+    custom_jobs: Any = custom_jobs_template.load(variables)
+    repo_dependencies.update(custom_jobs["templated"])
+    if custom_jobs["custom"]:
+        jobs.update(custom_jobs["custom"])
+
+    job_template = Template(PATH_JOB_TEMPLATE)
     for repo_name, dependencies in sorted(repo_dependencies.items()):
         dependencies.sort()
         variables = {
@@ -25,16 +39,13 @@ def main():
             "repo_name_short": repo_name.split("/")[1],
             "repo_owner": repo_name.split("/")[0],
             "deps_short": [dep_name.split("/")[1] for dep_name in dependencies],
-            "deps_json": json.dumps(dependencies)
+            "deps_json": json.dumps(dependencies),
         }
-        jobs.update(job_template.load(variables)) # type: ignore
+        jobs.update(job_template.load(variables))  # type: ignore
 
     nightly_template = Template(PATH_NIGHTLY_TEMPLATE)
-    variables = {
-        "jobs": jobs
-    }
+    variables = {"jobs": jobs}
     nightly_template.dump(variables, PATH_NIGHTLY)
-
 
 
 if __name__ == "__main__":

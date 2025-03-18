6ceccc3d4909b9e4c0702fa7a599240949ede5b3
Felix Schwickerath <felix@fschwickerath.de>
2025-03-18 14:39:24 +0100

    ci: fix directories for nightly build templates in script

--- scripts/generate_nightly/generate_nightly/__main__.py
+++ scripts/generate_nightly/generate_nightly/__main__.py
@@ -7,11 +7,11 @@ from typing import Any, Dict, List
 
 from .template.template import Template
 
-PATH_JOB_TEMPLATE = Path("template/job_template.yml")
-PATH_NIGHTLY_TEMPLATE = Path("template/nightly_template.yml")
-PATH_CUSTOM_JOBS_TEMPLATE = Path("template/custom_jobs_template.yml")
-PATH_FORCE_REBUILD_TEMPLATE = Path("template/force_rebuild_template.yml")
-PATH_NIGHTLY = Path(".github/workflows/nightly.yml")
+PATH_JOB_TEMPLATE = Path("../../template/job_template.yml")
+PATH_NIGHTLY_TEMPLATE = Path("../../template/nightly_template.yml")
+PATH_CUSTOM_JOBS_TEMPLATE = Path("../../template/custom_jobs_template.yml")
+PATH_FORCE_REBUILD_TEMPLATE = Path("../../template/force_rebuild_template.yml")
+PATH_NIGHTLY = Path("../../.github/workflows/nightly.yml")
 
 
 def main():
